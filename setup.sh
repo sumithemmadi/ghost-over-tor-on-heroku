6587088128612b033ed5f73b065875e022070906
Emmadi Sumith Kumar <sumithemmadi244@gmail.com>
2021-09-22 12:39:54 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-/*
+
 ## MIT License
 ## Copyright (c) 2021 Emmadi Sumith Kumar
 ## Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -16,7 +16,6 @@
 ## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 ## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ## SOFTWARE.
-*/
 
 rm -f ./temp/etc/tor/torrc
 
